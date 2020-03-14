ba3ef0511a7f7f08dfc46186046a8df7c4833356
SunYufei <sunyufei963@qq.com>
2020-03-14 15:01:16 +0800

    Update: 2020/03/14

---
+++ LeetCode/Java/22-backtracking-括号生成.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=22 lang=java
+ *
+ * [22] 括号生成
+ */
+
+// @lc code=start
+class Solution {
+    private List<String> res;
+
+    public List<String> generateParenthesis(int n) {
+        res = new ArrayList();
+        generate(0, 0, n, "");
+        return res;
+    }
+
+    private void generate(int left, int right, int n, String s) {
+        if (left == n && right == n) {
+            res.add(s);
+            return;
+        }
+
+        if (left < n)
+            generate(left + 1, right, n, s + "(");
+
+        if (left > right)
+            generate(left, right + 1, n, s + ")");
+    }
+}
+// @lc code=end
---
+++ LeetCode/Java/98-depth-first-search-验证二叉搜索树.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=java
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+
+    }
+}
+// @lc code=end
+
