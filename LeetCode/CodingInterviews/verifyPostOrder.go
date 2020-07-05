d4b92a93f39d2683654c909a4afedcee93a6b783
SunYufei <sunyufei963@qq.com>
2020-07-05 11:29:53 +0800

    Update: 2020/07/05

--- LeetCode/CodingInterviews/verifyPostOrder.go
+++ LeetCode/CodingInterviews/verifyPostOrder.go
@@ -4,27 +4,22 @@ func verifyPostorder(postorder []int) bool {
 	if len(postorder) == 0 {
 		return true
 	}
-	var check=func(left,right int)bool{
-		if left>right{
+	var check func(int, int) bool
+	check = func(left, right int) bool {
+		if left > right {
 			return true
 		}
-		var root=postorder[right]
-		var k=left
-		for k<right && post[k]<root{
+		var root = postorder[right]
+		var k = left
+		for k < right && postorder[k] < root {
 			k++
 		}
-		for i:=k;i<right;i++{
-			if postorder[i]<root{
+		for i := k; i < right; i++ {
+			if postorder[i] < root {
 				return false
 			}
 		}
-		if check(left,k-1)==false{
-			return false
-		}
-		if check(k,right-1)==false{
-			return false
-		}
-		return true
+		return check(left, k-1) && check(k, right-1)
 	}
-	return check(0,len(postorder)-1)
+	return check(0, len(postorder)-1)
 }
