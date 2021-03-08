bc7647a0ee125618365b48b2869affa221adf377
SunYufei <sunyufei963@qq.com>
2021-03-08 10:51:01 +0800

    Update: 2021/03/08

--- OUCDS/1.c
+++ OUCDS/1.c
@@ -1,188 +1,164 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// 结点定义
-typedef struct Node {
+typedef struct PolyNode* Polynomial;
+struct PolyNode {
     int coef;
-    int exp;
-    struct Node* next;
-} Node;
+    int expon;
+    Polynomial link;
+};
 
-// 链表定义
-typedef Node* List;
-
-Node* new_node() {
-    Node* res = (Node*)malloc(sizeof(Node));
-    res->next = NULL;
-    return res;
+void Attach(int c, int e, Polynomial* pRear) {
+    // 申请一个新结点
+    Polynomial P;
+    P = (Polynomial)malloc(sizeof(struct PolyNode));
+    P->coef = c;
+    P->expon = e;
+    P->link = NULL;
+    // 将结点添加到 pRear
+    (*pRear)->link = P;
+    *pRear = P;
 }
 
-List read_list() {
-    int n;
+Polynomial ReadPoly() {
+    int c, e, n;
+    // 定义一个含头结点链表
+    Polynomial P;
+    P = (Polynomial)malloc(sizeof(struct PolyNode));
+    P->link = NULL;
+    Polynomial Rear = P;
+    // 读入数据并尾插
     scanf("%d", &n);
-    // 含头结点链表
-    List res = new_node();
-    Node* p = res;
-    // 读数据
     while (n--) {
-        // 申请一个新的结点
-        Node* t = new_node();
-        // 读数据
-        scanf("%d %d", &(t->coef), &(t->exp));
-        // 添加到链表中
-        p->next = t;
-        p = p->next;
+        scanf("%d %d", &c, &e);
+        Attach(c, e, &Rear);
     }
     // 删除头结点
-    p = res;
-    res = res->next;
-    free(p);
-    return res;
+    Rear = P;
+    P = P->link;
+    free(Rear);
+    return P;
 }
 
-List add_list(List l1, List l2) {
-    // 含头结点链表
-    List res = new_node();
-    Node* p = res;
-    // 相加
-    while (l1 != NULL && l2 != NULL) {
-        Node* t = new_node();
-        if (l1->exp == l2->exp) {
-            // l1, l2 系数相同
-            t->coef = l1->coef + l2->coef;
-            t->exp = l1->exp;
-            l1 = l1->next;
-            l2 = l2->next;
-        } else if (l1->exp > l2->exp) {
-            // l1 系数 > l2
-            t->coef = l1->coef;
-            t->exp = l1->exp;
-            l1 = l1->next;
-        } else {
-            // l1 系数 < l2
-            t->coef = l2->coef;
-            t->exp = l2->exp;
-            l2 = l2->next;
-        }
-        if (t->coef == 0) {
-            // 零结点不尾插
-            free(t);
-        } else {
-            // 结点尾插
-            p->next = t;
-            p = p->next;
-        }
-    }
-    // 剩余，遍历尾插结点
-    Node* r = l1 != NULL ? l1 : l2;
-    while (r != NULL) {
-        Node* t = new_node();
-        t->coef = r->coef;
-        t->exp = r->exp;
-        p->next = t;
-        p = p->next;
-        r = r->next;
-    }
-    // 删除头结点
-    p = res;
-    res = res->next;
-    free(p);
-    return res;
-}
-
-List mul_list(List l1, List l2) {
-    if (l1 == NULL || l2 == NULL)
-        return NULL;
-    // 含头结点链表
-    List res = new_node();
-    Node *p = res, *p1, *p2;
-    // 使用 l1 第一项与 l2 相乘构造初始链表
-    p1 = l1, p2 = l2;
-    while (p2 != NULL) {
-        Node* t = new_node();
-        t->coef = p1->coef * p2->coef;
-        t->exp = p1->exp + p2->exp;
-        p->next = t;
-        p = p->next;
-        p2 = p2->next;
-    }
-    // l1 剩余项与 l2 相乘并与 res 相加
-    p1 = p1->next;
-    while (p1 != NULL) {
-        p2 = l2;
-        p = res;
-        while (p2 != NULL) {
-            int c = p1->coef * p2->coef;
-            int e = p1->exp + p2->exp;
-            // 定位到合适的点
-            while (p->next != NULL && p->next->exp > e)
-                p = p->next;
-            // 插入点
-            if (p->next != NULL && p->next->exp == e) {
-                // 消除 0 系数项
-                if (p->next->coef + c != 0) {
-                    p->next->coef += c;
-                } else {
-                    Node* t = p->next;
-                    p->next = t->next;
-                    free(t);
-                }
-            } else {
-                Node* t = new_node();
-                t->coef = c;
-                t->exp = e;
-                t->next = p->next;
-                p->next = t;
-                p = p->next;
-            }
-            p2 = p2->next;
-        }
-        p1 = p1->next;
-    }
-    // 删除头结点
-    p = res;
-    res = res->next;
-    free(p);
-    return res;
-}
-
-void print_list(List l) {
-    // 空链表输出 0
-    if (l == NULL) {
+void PrintPoly(Polynomial P) {
+    int flag = 0;
+    if (P == NULL) {
         printf("0 0\n");
         return;
     }
-    // 非空链表，递归输出
-    printf("%d %d", l->coef, l->exp);
-    if (l->next != NULL) {
-        printf(" ");
-        print_list(l->next);
-    } else {
-        printf("\n");
+    while (P != NULL) {
+        if (flag == 0) {
+            flag = 1;
+        } else {
+            printf(" ");
+        }
+        printf("%d %d", P->coef, P->expon);
+        P = P->link;
     }
+    printf("\n");
 }
 
-void delete_list(List l) {
-    if (l != NULL) {
-        delete_list(l->next);
-        free(l);
+Polynomial Add(Polynomial P1, Polynomial P2) {
+    // 定义含头结点链表
+    Polynomial P;
+    P = (Polynomial)malloc(sizeof(struct PolyNode));
+    P->link = NULL;
+    // 指向链表的指针
+    Polynomial Rear = P, t1 = P1, t2 = P2;
+    // 链表相加
+    while (t1 != NULL && t2 != NULL) {
+        if (t1->expon == t2->expon) {
+            if (t1->coef + t2->coef != 0)
+                Attach(t1->coef + t2->coef, t1->expon, &Rear);
+            t1 = t1->link;
+            t2 = t2->link;
+        } else if (t1->expon > t2->expon) {
+            Attach(t1->coef, t1->expon, &Rear);
+            t1 = t1->link;
+        } else {
+            Attach(t2->coef, t2->expon, &Rear);
+            t2 = t2->link;
+        }
     }
+    // 剩余部分遍历尾插
+    while (t1 != NULL) {
+        Attach(t1->coef, t1->expon, &Rear);
+        t1 = t1->link;
+    }
+    while (t2 != NULL) {
+        Attach(t2->coef, t2->expon, &Rear);
+        t2 = t2->link;
+    }
+    Rear->link = NULL;
+    // 删除头结点
+    Rear = P;
+    P = P->link;
+    free(Rear);
+    return P;
+}
+
+Polynomial Mult(Polynomial P1, Polynomial P2) {
+    if (P1 == NULL || P2 == NULL)
+        return NULL;
+    // 定义含头结点链表
+    Polynomial P;
+    P = (Polynomial)malloc(sizeof(struct PolyNode));
+    P->link = NULL;
+    // 指向链表的指针
+    Polynomial Rear, t, t1, t2;
+    Rear = P, t1 = P1, t2 = P2;
+    // 用 P1 的第一项乘以 P2，构造基础的 P
+    while (t2 != NULL) {
+        Attach(t1->coef * t2->coef, t1->expon + t2->expon, &Rear);
+        t2 = t2->link;
+    }
+    // P1 剩余项与 P2 相乘并与 P 相加
+    t1 = t1->link;
+    while (t1 != NULL) {
+        t2 = P2;
+        Rear = P;
+        while (t2 != NULL) {
+            int c = t1->coef * t2->coef;
+            int e = t1->expon + t2->expon;
+            // 定位找到合适的插入点
+            while (Rear->link != NULL && Rear->link->expon > e)
+                Rear = Rear->link;
+            // 插入点
+            if (Rear->link != NULL && Rear->link->expon == e) {
+                if (Rear->link->coef + c != 0) {
+                    Rear->link->coef += c;
+                } else {
+                    // 删除 0 系数项
+                    t = Rear->link;
+                    Rear->link = t->link;
+                    free(t);
+                }
+            } else {
+                t = (Polynomial)malloc(sizeof(struct PolyNode));
+                t->coef = c;
+                t->expon = e;
+                t->link = Rear->link;
+                Rear->link = t;
+                Rear = Rear->link;
+            }
+            t2 = t2->link;
+        }
+        t1 = t1->link;
+    }
+    // 删除头结点
+    Rear = P;
+    P = P->link;
+    free(Rear);
+    return P;
 }
 
 int main() {
-    List l1, l2, la, lm;
-    l1 = read_list();
-    l2 = read_list();
-    // mul
-    lm = mul_list(l1, l2);
-    print_list(lm);
-    // add
-    la = add_list(l1, l2);
-    print_list(la);
-    // delete
-    delete_list(l1);
-    delete_list(l2);
-    delete_list(la);
-    delete_list(lm);
+    Polynomial P1, P2, PP, PS;
+    P1 = ReadPoly();
+    P2 = ReadPoly();
+    PP = Mult(P1, P2);
+    PrintPoly(PP);
+    PS = Add(P1, P2);
+    PrintPoly(PS);
     return 0;
 }
