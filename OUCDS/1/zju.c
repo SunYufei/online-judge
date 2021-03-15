09b0e72d5a2550f46dc28be69e186deadd094f7e
SunYufei <sunyufei963@qq.com>
2021-03-15 10:13:07 +0800

    Update: 2021/03/15

--- OUCDS/1/zju.c
+++ OUCDS/1/zju.c
@@ -8,7 +8,7 @@ struct PolyNode {
     Polynomial link;
 };
 
-void Attach(int c, int e, Polynomial *pRear) {
+void Attach(int c, int e, Polynomial* pRear) {
     // 申请一个新结点
     Polynomial P;
     P = (Polynomial)malloc(sizeof(struct PolyNode));
